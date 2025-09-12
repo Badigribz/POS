2310c3d2a495267d9e73797d95866f03e1556db1
Gribz <gribzbadi@gmail.com>
2025-09-12 19:20:17 +0300

    Toasts in login.vue

--- pos-frontend/src/views/LoginView.vue
+++ pos-frontend/src/views/LoginView.vue
@@ -56,10 +56,12 @@
 import { ref } from 'vue';
 import axios from '../axios';
 import { useRouter } from 'vue-router'
+import { useToast } from 'vue-toastification';
 
 const email = ref('');
 const password = ref('');
 const router = useRouter();
+const toast = useToast();
 
 const login = async () => {
   try {
@@ -80,6 +82,8 @@ const login = async () => {
     // Set axios default header
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
+    toast.success('Login successful!');
+
     // Role-based redirect
     console.log('Login success:', res.data);
     if (role === 'admin') {
@@ -92,6 +96,13 @@ const login = async () => {
    // router.push('/dashboard') // ✅ redirect after login
   } catch (err) {
     console.error('Login error:', err.response);
+
+        if (err.response?.status === 422) {
+      toast.error('The email or password is incorrect. Please try again.');
+    } else {
+      toast.error('An error occurred. Please try again later.');
+    }
+
   }
 };
 </script>
